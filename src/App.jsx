081772dd8661a3752238e61789bf6727175425f7
Trevor <trevorhilz@gmail.com>
2024-02-28 15:03:28 -0600

    prepare for heroku deployment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import LandingPage from './components/LandingPage';
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
