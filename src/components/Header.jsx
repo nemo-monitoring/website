f1808182a454f5a6b373a22010c869cd28fa15a6
Trevor <trevorhilz@gmail.com>
2024-02-29 14:57:06 -0600

    added buttons, and the Using Nemo section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div id='header-box'>
-      {/* <img id='logo' src={nemoLogo} /> */}
       <img id='logo' src='../assets/nemo-logo.png' />
-      <div id='header-main'>
-        {/* <a
-          href='https://github.com/oslabs-beta/nemo'
-          target='_blank'
-          id='header-btn'
-        >
-          GitHub
-        </a> */}
-      </div>
     </div>
   );
 };
