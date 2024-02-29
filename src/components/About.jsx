6532d19bcd7f66a775f07a7f4bb5e493d6759e52
Trevor <trevorhilz@gmail.com>
2024-02-29 15:43:04 -0600

    changed header and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,43 +31,24 @@ const About = () => {
       </div>
       <h1 id='header'>Using Nemo</h1>
       <div id='about-use'>
-        <div id='use-text'>
-          {/* If you have already setup Nemo and have a locally run cluster, click
-          the button to access our online demo portal. If you do not have Nemo,
-          visit our GitHub page to learn more. */}
-          To get started with our tool, please visit our GitHub repository to
-          clone the backend server. This is essential for running the local
-          server that our frontend application relies on for serving its
-          information. Follow the steps below for a seamless setup:
-          <ol>
-            <li>
-              Navigate to our GitHub repository <em>(button on the right)</em>
-            </li>
-            <li>Clone the repository to your local machine.</li>
-            <li>
-              Follow the setup instructions in the README to get your server up
-              and running.
-            </li>
-          </ol>
-          Thank you for using our tool! If you encounter any issues or need
-          further assistance, please refer to our documentation or contact our
-          support team.
+        <div style={{ lineHeight: '1.5em' }}>
+          Nemo can be used as a standalone tool or in an online portal. To get
+          started with Nemo, please visit and clone our GitHub repository. This
+          is essential for running the local server that Nemo relies on for
+          serving its information whether accessed through the online portal or
+          as standalone service. If you encounter any issues or need further
+          assistance, please refer to our documentation or contact our support
+          team.
         </div>
-        <GitHubButton
-          href='https://github.com/oslabs-beta/nemo'
-          data-color-scheme='no-preference: light; light: light; dark: dark;'
-          data-size='large'
-          aria-label='Follow @buttons on GitHub'
-        >
-          Nemo on GitHub
-        </GitHubButton>
-        <a
-          href='https://nemo-dashboard.vercel.app/'
-          target='_blank'
-          id='header-btn'
-        >
-          Access Nemo
-        </a>
+        <div id='access-btn'>
+          <a
+            href='https://nemo-dashboard.vercel.app/'
+            target='_blank'
+            id='header-btn'
+          >
+            Access Nemo
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Header = () => {
   return (
     <div id='header-box'>
-      <img id='logo' src='../assets/nemo-logo.png' />
+      <a
+        id='logo'
+        href='https://www.youtube.com/watch?v=Hy8kmNEo1i8&ab_channel=ScatmanJohnOfficialYouTubeChannel'
+      >
+        <img src='../assets/nemo-logo.png' />
+      </a>
     </div>
   );
 };
