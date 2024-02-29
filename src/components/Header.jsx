d1358c1dc118c9eefa21c23e97328959277204dc
Trevor <trevorhilz@gmail.com>
2024-02-29 15:53:24 -0600

    removed link, fixed logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div id='header-box'>
-      <a
-        id='logo'
-        href='https://www.youtube.com/watch?v=Hy8kmNEo1i8&ab_channel=ScatmanJohnOfficialYouTubeChannel'
-      >
-        <img src='../assets/nemo-logo.png' />
-      </a>
+      <img id='logo' src='../assets/nemo-logo.png' />
     </div>
   );
 };
